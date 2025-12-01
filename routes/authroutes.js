2f6d86fb65398cc9e1e0ee093ed5b22d73007428
constructAbility <resourcecode12@gmail.com>
2025-12-01 18:40:36 +0530

    Update authroutes.js

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -1,14 +1,14 @@
 const express=require('express')
 
 const router=express.Router();
-
+const { ssoLogin } = require("../controllers/ssologin");
 const {register,login  }=require('../controllers/auth');
 
 
 
 router.post('/register',register);
 router.post('/login',login);
-
+router.post("/sso-login", ssoLogin);
 
 
 
